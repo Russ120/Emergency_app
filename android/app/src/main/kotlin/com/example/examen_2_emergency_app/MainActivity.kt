92ba8fb96a2e109d410894df7e057347c71a70a0
Rusbel Rodriguez <rusbelrodriguez50@gmail.com>
2024-03-16 13:22:13 -0400

    Inicio del proyecto

---
+++ android/app/src/main/kotlin/com/example/examen_2_emergency_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.examen_2_emergency_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/evento.dart
@@ -0,0 +1,27 @@
+class Evento {
+  // Caracteristicas / Propiedades
+  int id;
+  String title;
+  String description;
+  DateTime date;
+  String photo;
+
+  //constructor
+  Evento(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.date,
+      required this.photo});
+
+// la función toMap() toma un objeto de una clase y lo convierte en un mapa que puede ser fácilmente almacenado en una base de datos, lo que facilita su inserción, actualización o consulta.
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "title": title,
+      "description": description,
+      "date": date.toIso8601String(),
+      "photo": photo
+    };
+  }
+}
